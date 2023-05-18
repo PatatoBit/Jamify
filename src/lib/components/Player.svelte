46b2b6c89d3eb6365508c21c68557420b6d24b16
Patato <pathonbusiness@gmail.com>
2023-05-18 21:25:12 +0700

    basic embed with ending video

---
+++ src/lib/components/Player.svelte
@@ -0,0 +1,25 @@
+<script>
+	import Youtube, { PlayerState } from 'svelte-youtube';
+
+	export let src;
+
+	let last = src.substring(src.lastIndexOf('/') + 1, src.length);
+
+	console.log(last, src);
+
+	const options = {
+		height: '390',
+		width: '640',
+		playerVars: {
+			// https://developers.google.com/youtube/player_parameters
+			autoplay: 1
+		}
+	};
+
+	function onStateChange(event) {
+		console.table(event.detail.data);
+		if (event.detail.data === PlayerState.ENDED) alert('video ended');
+	}
+</script>
+
+<Youtube videoId={last} {options} on:stateChange={(event) => onStateChange(event)} />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import Player from '../lib/components/Player.svelte';
+</script>
+
+<Player src="https://youtu.be/Sm-Bpkf0ndA" />
